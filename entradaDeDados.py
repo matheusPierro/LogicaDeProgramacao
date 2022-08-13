8710c825cd317ae939ed42df02e9915d8e81d929
Matheus Ramos de Pierro <matheus.r.pierro@ericssoninovacao.com.br>
2022-08-13 13:59:15 -0300

    Entrada de dados com python

---
+++ entradaDeDados.py
@@ -0,0 +1,6 @@
+#Representa todo momento em que o usuario precisa interagir com o algoritmo
+# Pede a digitação do salário ao usuário
+salario = input("Digite o seu salário:")
+salario = float(salario)
+# Exibe o salário digitado
+print("Salário = R$", salario)
