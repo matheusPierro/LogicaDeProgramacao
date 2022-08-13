72b394ff535bbb0f4c658d7c6903cc01f45c37e6
Matheus Ramos de Pierro <matheus.r.pierro@ericssoninovacao.com.br>
2022-08-13 15:06:26 -0300

    Operadores Aritméticos com python

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,12 @@
+valor1 = 5
+valor2 = 3
+valor3 = 2
+resposta = valor1 + valor2 * valor2 	# Resulta 11
+resposta = (valor1 + valor2) * valor2	# Resulta 16
+
+valor1 = 17
+valor2 = 7
+resposta = valor1 % valor2		# Resulta 3
+resposta = valor1 // valor2		# Resulta 2
+resposta = valor1 / valor2		# Resulta 2.428
+resposta = 2 ** 4		        # Resulta 16
